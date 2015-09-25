498ec526f78adc0d345f19b1b5b5ea4e28369a96
igromov <igromov@ufs-online.ru>
2015-09-25 17:01:00 +0300

    pom

--- src/ru/ZDBTest/TestNG/StartAppiumTest.java
+++ src/ru/ZDBTest/TestNG/StartAppiumTest.java
@@ -1,10 +1,21 @@
 package ru.ZDBTest.TestNG;
 
-import io.appium.java_client.AppiumDriver;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class StartAppiumTest {
@@ -16,11 +27,22 @@ public class StartAppiumTest {
     private static final int MINIMUM = 0;
     private static final int MAXIMUM = 10;
 
-
+  @BeforeMethod
+  public void SetUp()   throws Exception {
+	  System.out.println("БЕфор");
+  										}
+  
+  @AfterMethod
+  public void tearDown() throws Exception {
+	  System.out.println("Афтер");
+  										}
+  
+  
   @Test
   public void f() {
 	  System.out.println("Запуск аппиум теста");
-	  
-	  
-  }
+	 
+  					}
+  					
+ 
 }
